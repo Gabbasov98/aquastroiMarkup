0d12ab0d95aac29890fc0c8107eac922e0373c80
Marsel Gabbasov <marsel13_98@mail.ru>
2021-09-25 15:47:30 +0500

    first commit

---
+++ footer.php
@@ -0,0 +1,71 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer__inner">
+            <div class="footer__left">
+                <div class="footer__col">
+                    <p class="footer__title">КОМПАНИЯ</p>
+                    <div class="footer__contacts">
+                        <div class="footer__contacts-left">
+                            <div class="footer__contacts-address">
+                                <span>наш офис:</span>
+                                <strong>Приморский край, г. Артём, <br> ул. Фрунзе, 45 стр. 9</strong>
+                            </div>
+                            <div class="footer__contacts-mail">
+                                <span>электронная почта:</span>
+                                <a href="mailto:info@aquastroy.ru">info@aquastroy.ru</a>
+                            </div>
+                        </div>
+                        <div class="footer__contacts-right">
+                            <span>телефоны:</span>
+                            <a href="tel:+79940033000">+7 994 003-30-00</a>
+                            <a href="tel:+79242511385">+7 924 251-13-85</a>
+                            <a href="tel:+79502910090">+7 950 291-00-90</a>
+                            <a href="tel:+74233743029">+7 (423-37) 4-30-29</a>
+                        </div>
+                    </div>
+                    <div class="footer__info">ООО "АкваСтрой" ИНН 2502046362 <br> © 2010-2021 Все права защищены. Копирование материалов с сайта <br> без активной ссылки запрещено.</div>
+                    <div class="footer__safe">Политика конфиденциальности</div>
+                    <p class="footer__by">
+                        Разработка сайта - <a href="http://www.kefirok.ru" target="_blank"> студия Кефирок</a>. 2021                 
+                    </p>
+                </div>
+            </div>
+            <div class="footer__right">
+                <div class="footer__col">
+                    <p class="footer__title">НАШИ УСЛУГИ</p>
+                    <div class="footer__items">
+                        <div class="footer__items-col">
+                            <a href="">Горизонтально направленное бурение</a>
+                            <a href="">Прокладка наружных трубопроводов траншейными методами</a>
+                            <a href="">Прокладка наружных трубопроводов бестраншейными методами</a>
+                            <a href="">Обустройство скважин</a>
+                            <a href="">Монтаж систем отопления</a>
+                            <a href="">Монтаж Локально очистных сооружений</a>
+                            <a href="">Монтаж внутренних систем водоснабжения и водоотведения</a>
+                            <a href="">Монтаж трубопроводной арматуры</a>
+                        </div>
+                        <div class="footer__items-col">
+                            <a href="">Монтаж гидротехнических сооружений</a>
+                            <a href="">Устройство монолитных сооружений</a>
+                            <a href="">Алмазное бурение</a>
+                            <a href="">Собственное производство ж/б изделий</a>
+                            <a href="">Проектирование систем инженерных сетей</a>
+                            <a href="">Поставка полного спектра материалов и оборудования для инженерных сетей</a>
+                            <a href="">Услуги токарного цеха</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</footer>
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/nouislider.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src='js/jquery.mCustomScrollbar.concat.min.js'></script>
+<script src="js/swiper.js"></script>
+<script src="js/index.js"></script>
---
+++ js/index.js
@@ -0,0 +1,179 @@
+function sliderMain() {
+    var swiper = new Swiper('.main .swiper-container', {
+        loop: true,
+        speed: 2000,
+        autoplay: {
+            delay: 1000,
+            // disableOnInteraction: true,
+        },
+        // speed: 1000,
+        // slidersPerView: 1,
+        // effect: "fade",
+        // navigation: {
+        //     nextEl: '.main .swiper-button-next',
+        //     prevEl: '.main .swiper-button-prev',
+        // },
+        effect: 'flip'
+    })
+}
+
+function gallerySlider1() {
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 9,
+        slidesPerView: 5,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 5,
+                spaceBetween: 0
+            },
+            768: {
+                slidesPerView: 5,
+                spaceBetween: 0
+            },
+        },
+    });
+    var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+}
+
+
+
+$(document).ready(function() {
+    gallerySlider1()
+    sliderMain()
+    $("select").niceSelect()
+    $(".advantages__items .container").mCustomScrollbar({
+        axis: "x",
+    });
+    $(".about-tabs__inner").mCustomScrollbar({
+        axis: "x",
+    });
+
+    $('input[type="tel"]').mask('+7 (999) 999-9999', { placeholder: '+7 (       )         -' });
+
+    $(".header__open-menu").click(function() {
+        $(this).addClass("header__open-menu--active")
+        $(".header__menu").show()
+    })
+
+    $(document).mouseup(function(e) { // событие клика по веб-документу
+        var div = $(".header__menu-inner"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            &&
+            div.has(e.target).length === 0) { // и не по его дочерним элементам
+            $(".header__menu").hide(); // скрываем его
+            $(".header__open-menu").removeClass("header__open-menu--active")
+        }
+    });
+
+    $(".nav__item-menu").hover(onIn, onOut);
+
+    $(".nav__item--open").click(function() {
+        if (window.innerWidth < 992) {
+            // $(".nav__item-hidden").hide()
+            $(this).toggleClass("nav__item--active");
+            $(this).siblings(".nav__item-dropdown").slideToggle()
+        }
+    })
+
+    $(".header__burger").click(function() {
+        $(this).toggleClass("header__burger--active")
+        $(".nav").slideToggle()
+    })
+
+    $(document).mouseup(function(e) { // событие клика по веб-документу
+        if (window.innerWidth < 992) {
+            var div = $(".nav"); // тут указываем ID элемента
+            if (!div.is(e.target) // если клик был не по нашему блоку
+                &&
+                div.has(e.target).length === 0) { // и не по его дочерним элементам
+                div.hide(); // скрываем его
+                $(".header__burger").removeClass("header__burger--active")
+            }
+        }
+
+    });
+
+
+    $(window).resize(function() { // задаем функцию при срабатывании события "resize" на объекте window
+        if (window.innerWidth < 1300) {
+            $("header .container").removeClass("container-big")
+        } else {
+            $("header .container").addClass("container-big")
+        }
+    });
+    if (window.innerWidth < 1300) {
+        $("header .container").removeClass("container-big")
+    } else {
+        $("header .container").addClass("container-big")
+    }
+
+
+
+    $(".modal4").on('shown.bs.modal', function() {
+        $(document).mouseup(function(e) { // событие клика по веб-документу
+            var div = $(".modal4 input"); // тут указываем ID элемента
+            if (!div.is(e.target) // если клик был не по нашему блоку
+                &&
+                div.has(e.target).length === 0) { // и не по его дочерним элементам
+                console.log("good")
+            }
+        });
+    })
+    $(document).mouseup(function(e) { // событие клика по веб-документу
+        var div = $(".modal4 input[type='text']"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            &&
+            div.has(e.target).length === 0) { // и не по его дочерним элементам
+
+        }
+        if (div.val() !== "") {
+            div.siblings("label").addClass("label--active")
+        } else {
+            div.siblings("label").removeClass("label--active")
+        }
+
+
+
+        var div2 = $(".modal4 input[type='tel'"); // тут указываем ID элемента
+        if (!div2.is(e.target) // если клик был не по нашему блоку
+            &&
+            div2.has(e.target).length === 0) { // и не по его дочерним элементам
+        }
+
+        if (div2.val().trim().length === 17) {
+            div2.siblings("label").addClass("label--active")
+        } else {
+            div2.siblings("label").removeClass("label--active")
+        }
+    });
+})
+
+function onIn() {
+    if (window.innerWidth > 992) {
+        let el = $(this)
+        setTimeout(function() {
+            if (el.is(':hover')) {
+                el.children(".nav__item-dropdown").show()
+            }
+
+        }, 200);
+    }
+}
+
+function onOut() {
+    if (window.innerWidth > 992) {
+        $(this).children(".nav__item-dropdown").hide()
+    }
+}
